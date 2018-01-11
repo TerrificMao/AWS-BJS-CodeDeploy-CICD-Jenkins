023271bf554bceaff42f4a3b72373d0d368dca5c
TerrificMao <maodanrong_buaa@163.com>
2018-01-11 23:53:03 +0800

    Green

--- index.php
+++ index.php
@@ -29,19 +29,20 @@
     <h1>BJS CodeDeploy CICD with Jenkins - V6</h1>
     <h2>Github + ALB + CodeDeploy + Jenkins</h2> 
     <h2>AWS BJS CodeDeploy Demo</h2>
-    <p>Please refer this link for more details. <a href="http://aws.amazon.com/documentation/codedeploy">AWS CodeDeploy Document</a>.</p>
-    <p>danrongm@amazon.com</a>.</p>
   </div>
-
+  </br>
   <div align="center">
   <?php
     $instance_id = @file_get_contents("http://instance-data/latest/meta-data/instance-id");
     $zone = @file_get_contents("http://169.254.169.254/latest/meta-data/placement/availability-zone");
   ?>
-    </br>
+    <img src="https://s3.cn-north-1.amazonaws.com.cn/bjsdemo/CodeDeploy-Application-Code/awschina.png" width="300"/>
     </br>    
     <h2>EC2 Instance ID: <?php echo $instance_id; ?></h2>
-    <h2>EC2 Zone: <?php echo $zone; ?></h2>    
+    <h2>EC2 Zone: <?php echo $zone; ?></h2>
+    </br>
+    <p>Please refer this link for more details. <a href="http://aws.amazon.com/documentation/codedeploy">AWS CodeDeploy Document</a>. Blue: #0188cc, Green: #00cc3a</p>
+    <p>danrongm@amazon.com</a>.</p>
   </div>    
 </body>
 </html>
