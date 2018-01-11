bfe2543a1a491b903f925edbbaf8750d7f026e5f
TerrificMao <maodanrong_buaa@163.com>
2018-01-11 17:17:25 +0800

    HTML change to PHP

--- index.php
+++ index.php
@@ -32,6 +32,29 @@
     <p>Please refer this link for more details. <a href="http://aws.amazon.com/documentation/codedeploy">AWS CodeDeploy Document</a>.</p>
     <p>danrongm@amazon.com</a>.</p>
   </div>
+
+  <div id="content">
+    <?php
+      # Get the instance ID from meta-data and store it in the $instance_id variable
+      $url = "http://169.254.169.254/latest/meta-data/instance-id";
+      $instance_id = file_get_contents($url);
+      # Get the instance's availability zone from metadata and store it in the $zone variable
+      $url = "http://169.254.169.254/latest/meta-data/placement/availability-zone";
+      $zone = file_get_contents($url);
+    ?>
+
+    <center>
+      <h2>EC2 Instance ID: <?php echo $instance_id; ?></h2>
+      <h2>Zone: <?php echo  $zone; ?></h2>    
+    </center>
+
+  </div>
+
+
+  <h4 class="">
+    
+
+  </h4>
 </body>
 </html>
 
