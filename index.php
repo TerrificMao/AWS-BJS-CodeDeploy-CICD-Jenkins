cc5e5adfe7fdefa45d2361635d470f382d3dc66e
TerrificMao <maodanrong_buaa@163.com>
2018-01-12 11:28:20 +0800

    V1

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 </head>
 <body>
   <div align="center">
-    <h1>BJS CodeDeploy CICD with Jenkins - V2</h1>
+    <h1>BJS CodeDeploy CICD with Jenkins - V1</h1>
     <h2>Github + ALB + CodeDeploy + Jenkins</h2> 
     <h2>AWS BJS CodeDeploy Demo</h2>
   </div>
