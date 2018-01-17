3c83ff0253b1eeb9aa498190af27a181467fa789
TerrificMao <maodanrong_buaa@163.com>
2018-01-17 10:49:46 +0800

    HSBC

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 </head>
 <body>
   <div align="center">
-    <h1>BJS CodeDeploy CICD with Jenkins - V1</h1>
+    <h1>AWS DevOps Demo for HSBC (Guangzhou) - V2</h1>
     <h2>Github + ALB + CodeDeploy + Jenkins</h2> 
     <h2>AWS BJS CodeDeploy Demo</h2>
   </div>
@@ -35,14 +35,32 @@
   <?php
     $instance_id = @file_get_contents("http://instance-data/latest/meta-data/instance-id");
     $zone = @file_get_contents("http://169.254.169.254/latest/meta-data/placement/availability-zone");
+    $private_id = @file_get_contents("http://instance-data/latest/meta-data/local-ipv4");
   ?>
     <img src="https://s3.cn-north-1.amazonaws.com.cn/bjsdemo/CodeDeploy-Application-Code/awschina.png" width="300"/>
     </br>    
     <h2>EC2 Instance ID: <?php echo $instance_id; ?></h2>
     <h2>EC2 Zone: <?php echo $zone; ?></h2>
+    <h2>EC2 Private IP: <?php echo $private_id; ?></h2>
     </br>
     <p>Please refer this link for more details. <a href="http://aws.amazon.com/documentation/codedeploy">AWS CodeDeploy Document</a>. Blue: #0188cc, Green: #00cc3a</p>
     <p>danrongm@amazon.com</a>.</p>
   </div>    
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
