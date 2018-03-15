ebea962cadde9eaa9e9f187ecd9cedb1dc38777f
TerrificMao <maodanrong_buaa@163.com>
2018-03-15 22:56:10 +0800

    Disneyland
    Disneyland

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 </head>
 <body>
   <div align="center">
-    <h1>AWS DevOps Demo for HSBC (Guangzhou) - V5</h1>
+    <h1>AWS DevOps Demo for Disneyland - V1</h1>
     <h2>Github + ALB + CodeDeploy + Jenkins</h2> 
     <h2>AWS BJS CodeDeploy Demo</h2>
   </div>
