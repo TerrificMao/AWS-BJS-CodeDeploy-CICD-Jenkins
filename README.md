c210ae2cf4e5e1a819f5f089814edfa90fcf1012
TerrificMao <maodanrong_buaa@163.com>
2018-08-07 20:58:11 +0800

    change to V3

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # AWS-BJS-CodeDeploy-CICD-Jenkins
 AWS-BJS-CodeDeploy-CICD-Jenkins
+https://aws.amazon.com/cn/blogs/china/aws-devops-jenkins-and-codedeploy/
--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 </head>
 <body>
   <div align="center">
-    <h1>AWS DevOps Demo for Disneyland - V2</h1>
+    <h1>AWS DevOps Demo for Disneyland - V3</h1>
     <h2>Github + ALB + CodeDeploy + Jenkins</h2> 
     <h2>AWS BJS CodeDeploy Demo</h2>
   </div>
